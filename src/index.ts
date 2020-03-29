82738835a08ed7900c31e47ff3678b5d6a593e84
smith-chris <kowalski.k@outlook.com>
2020-03-29 19:45:32 +0200

    Game runs off submodules source files!

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-import { Engine } from '@ash.ts/ts'
-import { EngineStateMachine, EntityStateMachine } from '@ash.ts/fsm'
+import { initialiseGame } from './JustShip'
 
-// import { initialiseGame } from './JustShip'
+window.addEventListener('load', async () => {
+  const containerElement = document.getElementById('game')
+  if (!containerElement) {
+    return
+  }
 
-// window.addEventListener('load', async () => {
-//   const containerElement = document.getElementById('game')
-//   if (!containerElement) {
-//     return
-//   }
-
-//   await initialiseGame(containerElement)
-// })
-console.log(Engine)
-const engine = new Engine()
-console.log(engine)
+  await initialiseGame(containerElement)
+})
